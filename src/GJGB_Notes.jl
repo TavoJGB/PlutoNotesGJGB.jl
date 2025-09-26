df043e6d58042ebaaac0e4b77f1ec8c82a745491
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-09-26 10:44:13 +0200

    Undo reexporting of PlutoUI
    I think it makes more sense to just add "using PlutoUI" whenever we will use it.

--- src/GJGB_Notes.jl
+++ src/GJGB_Notes.jl
@@ -3,16 +3,11 @@ module GJGB_Notes
     # Packages
     using HypertextLiteral: @htl, @htl_str # necesario para cita()
     using Markdown
-    using PlutoUI
-    using Reexport
 
     # Load dependencies
     include("./dep/Structs.jl")
     export Español, English, Français
     include("./dep/Aux_Functions.jl")
     export cita, resaltar, enlace, correcto, mejorable
-
-    # Reexport PlutoUI
-    @reexport using PlutoUI
     
 end

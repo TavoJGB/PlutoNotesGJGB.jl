00caa84471d2e049c6882706668182e9ef204d95
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-09-26 10:35:51 +0200

    Reexporting PlutoUI

--- src/GJGB_Notes.jl
+++ src/GJGB_Notes.jl
@@ -4,11 +4,15 @@ module GJGB_Notes
     using HypertextLiteral: @htl, @htl_str # necesario para cita()
     using Markdown
     using PlutoUI
+    using Reexport
 
     # Load dependencies
     include("./dep/Structs.jl")
     export Español, English, Français
     include("./dep/Aux_Functions.jl")
     export cita, resaltar, enlace, correcto, mejorable
+
+    # Reexport PlutoUI
+    @reexport using PlutoUI
     
 end

c951084f7291fe95bb3c77d65cef4b653a0e81dd
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-10-22 16:11:13 +0200

    Unordered lists

--- src/dep/Functions.jl
+++ src/dep/Functions.jl
@@ -82,7 +82,9 @@ enlace(texto, url) = """<a href="$url" target="_blank">$texto</a>"""
 
 # Generador de listas ordenadas a partir de un vector de items.
 # Acepta String, Markdown.MD o HTML para cada punto.
-function lista(puntos::AbstractVector{T}) where {T<:Union{String,Markdown.MD,HTML}}
+function lista(puntos::AbstractVector{T}; tipo::String="ol") where {T<:Union{String,Markdown.MD,HTML}}
+    @assert tipo ∈ ["ol", "ul"] "Tipo de lista '$tipo' no reconocido. Use 'ol' o 'ul'."
+
     # Normalizar cada elemento: si es String lo convertimos a HTML;
     # Markdown.MD y HTML se mantienen para que @htl los procese correctamente.
     items = [p isa String ? HTML(p) : p for p in puntos]
@@ -92,14 +94,14 @@ function lista(puntos::AbstractVector{T}) where {T<:Union{String,Markdown.MD,HTM
 
     # Devolver el HTML final como un bloque <ol>
     return @htl("""
-    <ol>
+    <$tipo>
         $(li_items...)
-    </ol>
+    </$tipo>
     """)
 end
 # Versión más permisiva que acepta cualquier vector y convierte a string cuando sea necesario
-lista(puntos::AbstractVector) = lista([p isa String ? p : (p isa Markdown.MD || p isa HTML ? p : string(p)) for p in puntos])
-lista(args...) = lista(collect(args))  # Permite pasar múltiples argumentos
+lista(puntos::AbstractVector; kwargs...) = lista([p isa String ? p : (p isa Markdown.MD || p isa HTML ? p : string(p)) for p in puntos]; kwargs...)
+lista(args...; kwargs...) = lista(collect(args); kwargs...)
 
 
 

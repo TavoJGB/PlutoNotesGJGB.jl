24ab77b73a81339f2c8fe3a705fccbf299252611
TavoJGB <116122159+TavoJGB@users.noreply.github.com>
2025-10-22 16:34:32 +0200

    List without indent

--- src/dep/Functions.jl
+++ src/dep/Functions.jl
@@ -82,7 +82,7 @@ enlace(texto, url) = """<a href="$url" target="_blank">$texto</a>"""
 
 # Generador de listas ordenadas a partir de un vector de items.
 # Acepta String, Markdown.MD o HTML para cada punto.
-function lista(puntos::AbstractVector{T}; tipo::String="ol") where {T<:Union{String,Markdown.MD,HTML}}
+function lista(puntos::AbstractVector{T}; tipo::String="ol", indent::Bool=true) where {T<:Union{String,Markdown.MD,HTML}}
     @assert tipo ∈ ["ol", "ul"] "Tipo de lista '$tipo' no reconocido. Use 'ol' o 'ul'."
 
     # Normalizar cada elemento: si es String lo convertimos a HTML;
@@ -92,13 +92,17 @@ function lista(puntos::AbstractVector{T}; tipo::String="ol") where {T<:Union{Str
     # Construir una lista de elementos <li> usando @htl para preservar tipos
     li_items = [ @htl("""<li>$(it)</li>""") for it in items ]
 
-    # Devolver el HTML final como un bloque <ol>
-    return @htl("""
-    <$tipo>
-        $(li_items...)
-    </$tipo>
-    """)
+    # Atributos para eliminar el margen izquierdo (cadena para insertar en la etiqueta)
+    attrhtml = indent ? "" : " style='margin:0;padding-left:0;margin-left:0;'"
+
+    # Construir la cadena HTML final concatenando los <li> ya procesados por @htl
+    # (li_items son objetos que al convertirlos a string devuelven su HTML)
+    inner = join(string.(li_items), "")
+    full = "<" * tipo * attrhtml * ">" * inner * "</" * tipo * ">"
+
+    return HTML(full)
 end
+
 # Versión más permisiva que acepta cualquier vector y convierte a string cuando sea necesario
 lista(puntos::AbstractVector; kwargs...) = lista([p isa String ? p : (p isa Markdown.MD || p isa HTML ? p : string(p)) for p in puntos]; kwargs...)
 lista(args...; kwargs...) = lista(collect(args); kwargs...)
